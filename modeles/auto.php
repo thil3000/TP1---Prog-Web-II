f2a030c5ea02d54011407da0ec079191d5690eb7
Thierry <thilega94@gmail.com>
2019-02-27 12:52:11 -0500

    modaphoqa

---
+++ modeles/auto.php
@@ -0,0 +1,20 @@
+<?php
+	function getCarList() {
+		return $tab_cars = array(
+			array("Make" => "123","Model" => "Model","Price" => "Price","Year" => "Year","Color" => "Color","KM" => "KMs"),
+			array("Make" => "456","Model" => "Model","Price" => "Price","Year" => "Year","Color" => "Color","KM" => "KMs"),
+		);
+	
+	}
+	
+	function getMakeList(){
+		$tab_cars = getCarList();
+		$tab_maker = array();
+		$i=0;
+		foreach ($tab_cars as $value) {
+			$tab_maker[$i] = $value["Make"];
+			$i++;
+		}
+		return $tab_maker;
+	}
+?>
--- vues/acceuil.php
+++ vues/acceuil.php
@@ -1,3 +1,9 @@
 <?php
-
+	function generateList() {
+		$tab_maker = getMakeList();
+		foreach ($tab_maker as $maker) {
+//			echo  $maker;
+			echo "<a href=\"index.php?make=$maker\">$maker</a>\n";	
+		}
+	}
 ?>

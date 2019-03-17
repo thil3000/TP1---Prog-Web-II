95b4216e5a17578a911c30847d4352662128377c
vince1016 <45361827+vince1016@users.noreply.github.com>
2019-03-16 22:28:03 -0400

    financementv1
    v1: a ameliorer et besoin de reverifier

--- controller/financementController.php
+++ controller/financementController.php
@@ -12,7 +12,18 @@
 	<body>	
 	<img class="banner" src="../images/bannerphp.php" alt="Baniere" width="50%">
 		<?php
-			//code	
+			function getCarValueWithAccount($carValue, $account){
+				return $carValue - $account;
+			}
+			function getMountlyCost($carValue, $durationInMounts, $interest){
+				$carFianalValuePerMounts = $carValue * (($interest(1+$interest) ) * exp($durationInMounts) / ((1+$interest) * exp($durationInMounts) - 1));
+				return $carFianalValuePerMounts;
+			}
+
+			function getInterest($carValuePerMount,$carValue,  $durationInMounts ,$account){
+				$interst = ($carValuePerMount * $durationInMounts) - getCarValueWithAccount($carValue, $account);
+				return $interst;
+			}
 		?>
 	</body>
 </html>
